141839cabebd90f09330cfead3eab25db838f2a3
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-04-08 22:00:08 -0300

    function resize done

--- ImagesConfigs/app.js
+++ ImagesConfigs/app.js
@@ -1,3 +1,6 @@
+const sharp = require('sharp')
+
+
 let path = process.argv[2]
 let width = Number(process.argv[3]) // passing to number cause command line receives always strings
 
@@ -6,6 +9,15 @@ console.log(path, width)
 function resize(path, width){
 
 
-    
+    sharp(path).resize({width}).toFile('./temp/resized_img.jpg', (err) => {
+        if (err){
+            console.log(err)
+        } else{
+            console.log("Imagem Redimensionada com Sucesso!")
+        }
+    })
+
 
 }
+
+resize(path, width)
