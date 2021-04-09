757bb4a0a18289e743517ba4616ded6498941250
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-04-08 21:43:54 -0300

    structure to take data from terminal done

---
+++ ImagesConfigs/app.js
@@ -0,0 +1,11 @@
+let path = process.argv[2]
+let width = Number(process.argv[3]) // passing to number cause command line receives always strings
+
+console.log(path, width)
+
+function resize(path, width){
+
+
+    
+
+}
